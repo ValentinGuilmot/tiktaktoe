7c3a742eb6fce8d89bcba1465e60e84a1bd57441
ValentinGuilmot <guilmot.valentin@gmail.com>
2022-01-15 10:12:45 -0800

    display board

---
+++ main.py
@@ -0,0 +1,14 @@
+def display_board():
+    input1=' '
+    input2=' '
+    input3=' '
+    input4=' '
+    input5=' '
+    input6=' '
+    input7=' '
+    input8=' '
+    input9=' '
+
+    board=f'|{input7}|{input8}|{input9}|\n-------\n|{input4}|{input5}|{input6}|\n-------\n|{input1}|{input2}|{input3}|' 
+    print(board)
+display_board()
